8342ee40f17081d0dc1b0bf9c33346f36827b538
Max <hi.max@foxmail.com>
2023-12-16 14:05:42 +0800

    feat: add Schema for GasReportDomain

--- src/ethereumWatcher/domain/ethereumWatcher.domain.ts
+++ src/ethereumWatcher/domain/ethereumWatcher.domain.ts
@@ -1,9 +1,16 @@
 import { AutoMap } from '@automapper/classes';
 import { AggregateRoot } from '@nestjs/cqrs';
-import { Prop } from '@nestjs/mongoose';
+import { Prop, Schema } from '@nestjs/mongoose';
 import { Target, GasReport } from './ethereumWatcher.valueobject';
 
+@Schema({
+    collection: GasReportDomain.collectionName,
+    timestamps: true,
+    optimisticConcurrency: true,
+})
 export class GasReportDomain extends AggregateRoot {
+    public static collectionName = 'gasReports';
+
     @Prop()
     @AutoMap()
     public id: string;
